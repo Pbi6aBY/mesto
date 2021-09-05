109b3048a51b34ed5ea1a411b5045ef2bfc2cf1d
Andrey Ryabchuk <andrey.riabchuk@yandex.by>
2021-09-05 17:31:58 +0300

    вторая проверка

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ let savePopupButton = document.querySelector(".popup__save");
 let closePopupButton = document.querySelector(".popup__close");
 let profileTitle = document.querySelector(".profile__title");
 let profileSubtitle = document.querySelector(".profile__subtitle");
-let nameInput = document.querySelector(".popup__text-name");
-let jobInput = document.querySelector(".popup__text-about");
+let nameInput = document.querySelector(".popup_type_name");
+let jobInput = document.querySelector(".popup_type_about");
 
 function togglePopup() {
-  if (!popup.classList.contains("popup_opened")) {
-  }
   popup.classList.toggle("popup_opened");
 }
 
